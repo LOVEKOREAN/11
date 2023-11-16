976b69b096c57dea308d7bc87ec32a9a92cf5985
LOVEKOREAN <JIWON0521@SOOKMYUNG.AC.KR>
2023-11-16 16:06:03 +0900

    05
    실습 5

--- main.c
+++ main.c
@@ -3,23 +3,21 @@
 
 int main(int argc, char *argv[])
 {
-    int i;
-    int grade[5];
-    int average=0;
-
-    for (i=0;i<5;i++)
-    {
-      printf("input valur [%i]=",i);
-      scanf("%d",&grade[i]);
-    }
+  char *pc;
+  int *pi;
+  double *pd;
+  
+  pc=(char*)10000;
+  pi=(int*)10000;
+  pd=(double*)10000;
     
-    for(i=0;i<5;i++)
-    {
-       printf("grade[%i]=%i\n",i,*(grade+i));
-       average = average +*(grade+1);
-    }
-       printf("average:%i\n",average/5);
-
+  printf("증가 전:pc=%d, pi=%d, pd=%d\n", pc,pi,pd);
+  
+  pc++;
+  pi++;
+  pd++;
+  printf("증가 후:pc=%d, pi=%d, pd=%d\n", pc,pi,pd);
+  
     
   system("PAUSE");	
   return 0;
